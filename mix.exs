c17acf65232d4dc4f12f38f1851d89d12273e90e
meryldakin <meryl.k.dakin@gmail.com>
2018-09-05 15:45:54 -0400

    making hex package

--- mix.exs
+++ mix.exs
@@ -8,7 +8,13 @@ defmodule Encrypt.MixProject do
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      escript: escript()
+      escript: escript(),
+      name: "encrypt",
+      organization: "flatironschool",
+      lisences: ["MIT License"],
+      links: %{"Github" => "https://github.com/flatiron-labs/encrypt"},
+      source_url: "https://github.com/flatiron-labs/encrypt",
+      homepage_url: "https://github.com/flatiron-labs/encrypt"
     ]
   end
 
@@ -23,13 +29,10 @@ defmodule Encrypt.MixProject do
     [main_module: Encrypt.Cli]
   end
 
-
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:ex_doc, "~> 0.19", only: :dev, runtime: false}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
   end
 end
